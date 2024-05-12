da4f8dfbb7ab40db44289f8eac0618b0fb3a3d88
RajeshB4134 <bandarurajesh4134@gmail.com>
2024-05-12 15:55:43 +0530

    update

--- component/TextForm.jsx
+++ component/TextForm.jsx
@@ -1,27 +1,52 @@
 import React, { useState } from "react";
 
-
-
 const TextForm = (props) => {
-    const handleUpClick = ()=>{
-        console.log("Converted into upperCase " + text)
-        let newText = text.toUpperCase();
-        setText(newText)
-    }
-    const handleonChange = (event)=>{
-        setText(event.target.value)
-    }
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleonChange = (event) => {
+    setText(event.target.value);
+  };
   const [text, setText] = useState("");
   return (
-    <div>
-      <div className="m-3">
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-          <textarea className="form-control" onChange={handleonChange} id="myBox" value={text} rows="8"></textarea>
+    <>
+      <div className="container">
+        <div className="m-3">
+          <h1>{props.heading}</h1>
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              onChange={handleonChange}
+              id="myBox"
+              value={text}
+              rows="8"
+            ></textarea>
+          </div>
+          <button className="btn btn-primary" onClick={handleUpClick}>
+            Convert to Uppercase
+          </button>
+          <button className="btn mx-3 btn-primary" onClick={handleLoClick}>
+            Convert to Lowercase
+          </button>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
       </div>
-    </div>
+      <div className="container m-3">
+        <h1>Text Analysis</h1>
+        <p>
+          {text.split(" ").length}words and {text.length} characters
+        </p>
+        <p>
+            {0.008 * text.split(" ").length}Minutes Read
+        </p>
+        <h1>Preview</h1>
+        <p>{text}</p>
+      </div>
+    </>
   );
 };
 
